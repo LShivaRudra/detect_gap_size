28d3d0964a92feae58e91ed47ce1570b6517d104
LShivaRudra <shivarudralolla@gmail.com>
2023-12-20 03:53:14 +0530

    changed the bounding rect function

--- src/test3.py
+++ src/test3.py
@@ -83,40 +83,42 @@ try:
                 max_contour = contour
 
         if max_contour is not None:
-            # Get the bounding rectangle of the max_area contour
-            x, y, w, h = cv2.boundingRect(max_contour)
+            # Get the minimum bounding rectangle of the max_area contour
+            rect = cv2.minAreaRect(max_contour)
 
-            cx_gap_box = int(x+w/2)
-            cy_gap_box = int(y+h/2)
-            tol_to_align_centers = 50 # This tolerance is used to check if the center of the max_area rectangle is aligning with that of ROI
+            # Extract the rectangle parameters
+            (cx_gap_box, cy_gap_box), (w, h), angle = rect
 
-            tol = 50 # This tolerance is used to check if there is any obstacle in front of the drone within 2 m. If that space is clear, the drone can pass through without checking for the remaining conditions for the current frame
-            
-            if(abs(w-roi_im_b)<tol and abs(h-roi_im_h)<tol): # Condition to check if dimensions of max_area rectangle are close to ROI in the current img frame
-                print("It is a freespace ahead! The drone can pass through freely")
+            tol_to_align_centers = 50  # This tolerance is used to check if the center of the max_area rectangle is aligning with that of ROI
+            tol = 50  # This tolerance is used to check if there is any obstacle in front of the drone within 2 m. If that space is clear, the drone can pass through without checking for the remaining conditions for the current frame
 
-            elif(abs(cx_gap_box-cx)<tol_to_align_centers and abs(cy_gap_box-cy)<tol_to_align_centers): # centers are aligning
-                if(w>drone_im_b and h>drone_im_h):
-                    print("The drone can pass safely without morphing") # If gap dimensions are more than that of the drone, move forward
+            if abs(w - roi_im_b) < tol and abs(h - roi_im_h) < tol:  # Condition to check if dimensions of max_area rectangle are close to ROI in the current img frame
+                print("It is a free space ahead! The drone can pass through freely")
+
+            elif abs(cx_gap_box - cx) < tol_to_align_centers and abs(cy_gap_box - cy) < tol_to_align_centers:  # centers are aligning
+                if w > drone_im_b and h > drone_im_h:
+                    print("The drone can pass safely without morphing")  # If gap dimensions are more than that of the drone, move forward
                 else:
-                    print("The drone has to morph") # If gap dimensions are less than that of the drone, morph
+                    print("The drone has to morph")  # If gap dimensions are less than that of the drone, morph
 
-            elif(abs(cx_gap_box-cx)>tol_to_align_centers and abs(cy_gap_box-cy)<tol_to_align_centers): # x-coordinate of the centers are not aligning
-                if((cx_gap_box-cx)<0):
-                    print("Try turning the drone towards left to check for better window to pass through")
-                
-                elif((cx_gap_box-cx)>0):
-                    print("Try turning the drone towards right to check for better window to pass through")
+            elif abs(cx_gap_box - cx) > tol_to_align_centers and abs(cy_gap_box - cy) < tol_to_align_centers:  # x-coordinate of the centers are not aligning
+                if (cx_gap_box - cx) < 0:
+                    print("Try turning the drone towards left to check for a better window to pass through")
 
-            elif(abs(cx_gap_box-cx)<tol_to_align_centers and abs(cy_gap_box-cy)>tol_to_align_centers): # y-coordinate of the centers are not aligning
-                if((cy_gap_box-cy)<0):
-                    print("Try turning the drone upwards to check for better window to pass through")
-                
-                elif((cy_gap_box-cy)>0):
-                    print("Try turning the drone downwards to check for better window to pass through")
+                elif (cx_gap_box - cx) > 0:
+                    print("Try turning the drone towards right to check for a better window to pass through")
 
-            # Draw the maximum rectangle on the image
-            cv2.rectangle(bg_info_image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            elif abs(cx_gap_box - cx) < tol_to_align_centers and abs(cy_gap_box - cy) > tol_to_align_centers:  # y-coordinate of the centers are not aligning
+                if (cy_gap_box - cy) < 0:
+                    print("Try turning the drone upwards to check for a better window to pass through")
+
+                elif (cy_gap_box - cy) > 0:
+                    print("Try turning the drone downwards to check for a better window to pass through")
+
+            # Draw the minimum bounding rectangle on the image
+            box = cv2.boxPoints(rect)
+            box = np.int0(box)
+            cv2.drawContours(bg_info_image, [box], 0, (0, 255, 0), 2)
 
         cv2.rectangle(bg_info_image, (roi_x1, roi_y1), (roi_x2, roi_y2), (255, 255, 255), 2)
         cv2.imshow('Original Color', color_image)
